d19a44ecada5ef81de8209d2c5ab368de0b34aaf
troyxxf <74029922+troyxxf@users.noreply.github.com>
2022-03-15 23:17:37 +0800

    Update CodeCraft-2022-charger.py

--- CodeCraft-2022-charger.py
+++ CodeCraft-2022-charger.py
@@ -35,7 +35,7 @@ def readDemand(dir):
         Client=[client_info for i in range(Client_Count)]
         for i in range(1,len(p_tmp)):
             Client[i-1]=client_info(p_tmp[i])
-        Time_count=-1
+        Time_count=0
         while True:
             lines = file_to_read.readline()  # 整行读取数据
             if not lines:
@@ -74,17 +74,21 @@ def readSolution(dir):
                 # lines[0]=lines.split(":<")
 
                 if not lines:
-                    #TODO：说明数据量与计费点*用户数不符合，需要报错
                     print("solution差数据")
                     break
                     pass
-
+                given = 0
                 p_tmp=lines.split(">,<")
                 for i in range(len(p_tmp[0])):
                     if p_tmp[0][i] == ":":
                         name = p_tmp[0][:i]
                         p_tmp[0]=p_tmp[0][i+2:]
                         break
+                if name != Client[client].name:
+                    print(name,"名字不对,应该是",Client[client].name)
+                    exit()
+                if(len(lines)==len(name)+1):
+                    continue
                 #删除回车和<>等符号
                 lastone=p_tmp[len(p_tmp)-1]
                 if lastone[len(lastone)-2:]=="\n":
@@ -92,37 +96,49 @@ def readSolution(dir):
                 elif lastone[len(lastone)-1:]==">":
                     lastone=lastone[:len(lastone)-1]
                 p_tmp[len(p_tmp)-1]=lastone
-                given=0
+
+                print(Client[client].name,day)
                 for i in p_tmp:
                     one_tmp=i.split(",")
+                    flag=0
                     for site_id in range(len(Site)):
                         if(Site[site_id].sitename==one_tmp[0]):#找节点的名字
+                            flag=1
                             if(Site[site_id].delayTime[client]>qos_constraint):
-                                print(Client[client].name,"的qos未被满足，它发给了",Site[site_id])
+                                print(Client[client].name,"的qos未被满足，它发给了",Site[site_id].sitename)
                                 exit()
                             Site[site_id].used[day]+=int(one_tmp[1])
+
+                            print(given)
                             given+=int(one_tmp[1])
+                            print(one_tmp[1])
+                            print(given)
                             if Site[site_id].used[day]>Site[site_id].bandwidth:
                                 print(Site[site_id].sitename,"的负载超过了带宽")
                                 print(Site[site_id].used)
                                 print(Site[site_id].bandwidth)
                                 exit()
-
-            for cc in Client:
-                if cc.name==name:
-                    if given<cc.demand[day]:
-                        print(cc.name,"的第",day,"个点没有满足需求")
-                        print("分发了",given,"需求，需要",cc.demand[day])
-                        exit()
+                            break
+                    if(flag==0):
+                        print("在时刻",day,"没有这个节点",one_tmp[0])
+                if given!=Client[client].demand[day]:
+                    print(Client[client].name, "的第", day, "个记录点没有满足需求")
+                    print("分发了", given, "需求，需要", Client[client].demand[day])
+                    exit()
+        lines = file_to_read.readline()
+        if not lines:
+            print("solution刚好读完")
+        else:
+            print("怎么读出多一行了")
+            print(lines)
 
 def billing():
     sum=[]
     for i in range(len(Site)):
         charge_95=sorted(Site[i].used)
-        point=math.ceil(len(charge_95)*0.95)-1
+        point=math.ceil(len(charge_95)*0.95)-2
         sum.append(charge_95[point]*Site[i].price)
     return sum
-
 
 
 if __name__ == '__main__':
@@ -138,9 +154,8 @@ if __name__ == '__main__':
     readQos(qosFile)
     readSolution(solutionFile)
     price=billing()
-    print(price)
+    # print(price)
     print(sum(price))
-
 
 
 
